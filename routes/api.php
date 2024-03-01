ab05edb981b0adfdfa58121b62b84c78664eefbd
Vila71 <lolivila71@gmail.com>
2024-03-01 12:58:33 +0100

    create Api rutes intolerances category

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\IntoleranceController;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(CategoryController::class)->group(function() {
+    Route::get('/categories',  'index' );
+     
+});
+
+Route::controller(IntoleranceController::class)->group(function() {
+    Route::get('/intolerances',  'index' );
+});  
